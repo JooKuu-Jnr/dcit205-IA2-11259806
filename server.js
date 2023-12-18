a15456dda5c7748df3d2c1bfe32764aea36b5804
Kuuku Ampah <ampah.kuuku@gmail.com>
2023-12-18 09:26:44 +0000

    Created endpoint for patient data

--- server.js
+++ server.js
@@ -3,5 +3,25 @@ const mongoose = require('mongoose')
 const Patient = require ('./models/patientModel')
 const app = express ()
 
-console.log('Hello');
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+app.get('/', (req, res) =>{
+    res.send('Hello Dcit205')
+})
+
+app.get('/blog', (req, res) =>{
+    res.send('Hello,could not  install npm due to no fault of mine')
+})
+
+// get data from database
+app.get('/patients', async(req, res)=>{
+    try {
+        const patients = await Patient.find({});
+        res.status(200).json(patients);
+    } catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
 
